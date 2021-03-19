484f2a56a988d359646f038867e6c88db8bbd778
Julio Herrera <jurhs2000@gmail.com>
2021-03-19 17:44:36 -0600

    fix: ESLint problems fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,26 +18,9 @@ module.exports = {
     'html'
   ],
   rules: {
-    'no-extra-semi': 'off',
-    'semi': 'off',
+    semi: ['error', 'never'],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': 'off',
-    'linebreak-style': 'off',
-    'no-undef': 'off',
-    'indent': 'off',
-    'arrow-body-style': 'off',
-    'react/jsx-indent': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'no-use-before-define': 'off',
-    'react/button-has-type': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'react/jsx-indent-props': 'off',
-    'react/destructuring-assignment': 'off',
-    'jsx-a11y/no-noninteractive-tabindex': 'off',
-    'jsx-a11y/tabindex-no-positive': 'off',
-    'no-console': 'off'
+    'no-console': 'off',
   },
-};
+}
